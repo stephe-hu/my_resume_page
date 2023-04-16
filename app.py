b59d97d6d91ffd086d4adc336718d1a29c502588
stephe-hu <stephen.hu@stonybrook.edu>
2023-04-16 22:47:35 +0000

    i have added to the home.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html')
 
-app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=5000, debug=True)
